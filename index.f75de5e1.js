65811cdd052824c5fa717ff6b3a720708ca01b79
DESKTOP-EHDJ2FAyarik <yarik.zubyk@gmail.com>
2024-05-02 19:45:28 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", ()=>{
+    toglleMenu();
+});
+const toglleMenu = ()=>{
+    const menuToggle = document.querySelector(".header .header__buttons-toggle");
+    const body = document.querySelector("body");
+    const menuNav = document.querySelector("#menu");
+    const allLinks = document.querySelectorAll(".menu .link-hovered");
+    if (!menuToggle) return;
+    menuToggle.addEventListener("click", (el)=>{
+        closeMenu(body, menuNav, menuToggle);
+    });
+    allLinks.forEach((element)=>{
+        element.addEventListener("click", (el)=>{
+            closeMenu(body, menuNav, menuToggle);
+        });
+    });
+};
+const closeMenu = (body, menuNav, menuToggle)=>{
+    body.classList.toggle("body--fixed");
+    menuNav.classList.toggle("menu--active");
+    menuToggle.classList.toggle("header__buttons-toggle--active");
+};
+
+//# sourceMappingURL=index.f75de5e1.js.map
